86ca9eb4423dcd2721ac182e1906c47e0582dae7
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-17 15:48:11 +0200

    call API

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 
 <script>
+import axios from "axios"
 export default {
   data () {
     
 
-    return {}
-  }
+    return {
+      API_URL: "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+    }
+  },
+   methods: {
+    callAPI(url) {
+      axios 
+        .get(url)
+        .then(response => {
+          console.log(response.data);
+        })
+
+    }
+   }, 
+   mounted() {
+    this.callAPI(this.API_URL)
+   }
 }
+
 </script>
+
 
 <template>
   <div>
-
+    <h1>YugiOh
+    </h1>
   </div>
 </template>
 
